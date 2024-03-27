808fd4cb97ff8a007238146f0a918c3b3f061c66
Aung Pyae Phyo <aungpyaephyoe.dev@gmail.com>
2024-03-27 22:34:11 +0630

    Initial State

---
+++ app/Livewire/Clicker.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\User;
+use Livewire\Attributes\Rule;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class Clicker extends Component
+{
+    use WithPagination;
+    
+    #[Rule('required|min:2|max:50')]
+    public $name = '';
+
+    #[Rule('required|email|unique:users')]    
+    public $email = '';
+
+    #[Rule('required|min:2')]
+    public $password = '';
+
+    public function render()
+    {
+        $users = User::paginate(5);
+
+        return view('livewire.clicker', [
+            'users' => $users
+        ]);
+    }
+
+    public function createNewUser() {
+
+        $this->validate();
+
+        User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => $this->password
+        ]);
+
+        $this->reset(['name', 'email', 'password']);
+
+        request()->session()->flash('success', 'User created successfully');
+    }
+}
